6450f72ae280faea58a1f897015ad09ae3d00407
Alexander Dahl <post@lespocky.de>
2012-03-06 17:31:59 +0100

    initital

---
+++ src/baz.c
@@ -0,0 +1,24 @@
+/*******************************************************************//**
+ *	@file	baz.c
+ *
+ *	baz test prog calling libbar which itself calls libfoo
+ *
+ *	@author	Alexander Dahl <post@lespocky.de>
+ *	@date	Created: 2012-03-06
+ **********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bar.h"
+
+int main( int argc, char *argv[] ) {
+
+	(void) puts( "This is baz." );
+	(void) puts( "Next line is from bar library:" );
+	bar_hello();
+
+	return EXIT_SUCCESS;
+}
+
+/* vim: set noet sts=0 ts=4 sw=4 sr: */
